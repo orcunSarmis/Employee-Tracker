c00172ed11159ebc0bfafbda47f5f70a3086b3d5
Orcun Sarmis <yorcunsarmis@gmail.com>
2022-05-28 00:59:53 +1000

    Schema and seeds statements added.

--- Main/index.js
+++ Main/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 //  Import and require mysql2
 const mysql2 = require('mysql2');
 const inquire = require('inquire');
+// Import and require console.table
+const cTable = require('console.table');
+const Connection = require('mysql2/typings/mysql/lib/Connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -24,3 +27,22 @@ const db = mysql.createConnection(
     console.log(`Connected to the database.`)
 );
 
+Connection.connect((err) => {
+    if (err) throw err;
+    ();
+});
+
+const = () => {
+    inquirer.propmt([
+        {
+            name: '',
+            type: '',
+            message: '',
+
+        }
+    ])
+}
+
+app.listen(PORT, () => {
+    console.log(`Server on port ${PORT}`);
+});
